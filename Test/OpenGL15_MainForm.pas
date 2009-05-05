3759ee727287aafe681378fc5cbef31ef297d590
mvdhoning <mvdhoning@users.noreply.github.com>
2009-05-05 12:43:02 +0000

    Changed Example to reflect my new development environment. Reminder make paths relative!

--- Test/OpenGL15_MainForm.pas
+++ Test/OpenGL15_MainForm.pas
@@ -250,13 +250,13 @@ scene1.AddModel();
 //mesh1.TexturePath:='textures\';  // no use at this point...
 
 //scene1.Models[0].LoadFromFile('models\tulip.3ds');  //Yeah it works again!!!...
-//scene1.Models[0].LoadFromFile('models\hog2.txt');
+scene1.Models[0].LoadFromFile('models\hog2.txt');
 //scene1.Models[0].LoadFromFile('models\soccerball.obj');
 //scene1.Models[0].LoadFromFile('models\trashbin.obj');
 //scene1.Models[0].LoadFromFile('models\houseobjtexwin.obj');
 //scene1.Models[0].LoadFromFile('models\housewiththickwalls.obj');
 //scene1.Models[0].LoadFromFile('models\houseobj.x');
-scene1.Models[0].LoadFromFile('c:\test\accel\cell.x');
+//scene1.Models[0].LoadFromFile('c:\test\accel\cell.x');
 
 //scene1.AddModel; //this gives error on calculation normals after loading soccerball.obj
 //scene1.Models[1].LoadFromFile('models\tulip.3ds'); //was tulip.3ds
@@ -268,7 +268,7 @@ scene1.Models[0].LoadFromFile('c:\test\accel\cell.x');
 //mesh1.LoadFromFile('models\character2.txt'); //Load the mesh...
 //mesh1.LoadFromFile('models\bend.txt');
 
-//scene1.Models[0].TexturePath:='textures\'; //set texturepath again since it is lost...
+scene1.Models[0].TexturePath:='textures\'; //set texturepath again since it is lost...
 //scene1.Models[1].TexturePath:='models\';
 
 //TMsaModel(mesh1).SaveToFile('tulip.txt');
